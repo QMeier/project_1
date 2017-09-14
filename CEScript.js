eb102bbbd96937a846cf619c12e1f0ba79fb1a16
Chong Tan <a46981@gmail.com>
2017-09-13 19:24:52 -0500

    Change to another function to save the data to local storage
    The sessionStorage function cannot save   data after you close the window so we change to local storage.

--- CEScript.js
+++ CEScript.js
@@ -1,31 +1,102 @@
-window.addEventListener("load", doFirst, false);
-function doFirst(){
-	var button = document.getElementById("button");
-	button.addEventListener("click", saveCrap, false);
-	display();
+window.addEventListener("load", displayCreateEvent, false);
+// function doFirst(){
+// 	var button = document.getElementById("button");
+// 	button.addEventListener("click", saveCrap, false);
+// 	display();
+// }
+// function saveCrap(){
+// 	var name = document.getElementById("name").value;
+// 	sessionStorage.setItem(name,"234");
+// 	document.getElementById('name').value = "";
+// 	display();
+// }
+
+// function display(){
+// 	var Eventlist = document.getElementById("Eventlist");
+// 	Eventlist.innerHTML = "We are ready have following event(s):" +"<br />";
+// 	for(var x=0;x<sessionStorage.length;x++)
+// 	{
+// 		var EN = sessionStorage.key(x);
+// 		var D = sessionStorage.getItem(EN);
+// 		Eventlist.innerHTML += (x+1) + ". Event:" + EN + ". Time:" + D + "<br />";
+// 	}
+// }
+var eventArray = [];
+// function init(){
+// 	if(localStorage.eventRecord)
+// 	{
+// 		eventArray = JSON.parse(localStorage.eventRecord);
+// 		for(var i=0;i<eventArray.lengthl;i++)
+// 		{
+
+// 		}
+// 	}
+// }
+// function save(name, year, month, date, time){
+// 	var n = document.getElementById(name).value;
+// 	var y = document.getElementById(year).value;
+// 	var m = document.getElementById(month).value;
+// 	var d = document.getElementById(date).value;
+// 	var t = document.getElementById(time).value;
+// 	var eventObj = {name:n, year:y, month:m, date:d, time:t};
+// 	eventArray.push(eventObj);
+// 	localStorage.eventRecord = JSON.stringify(eventArray);
+// 	//var fs = require("fs");
+// 	//fs.writeFile('write.txt',eventObj.name);
+// 	//document.cookie = eventObj.name+"expires=Fri, 18 Dec 2019 UTC; path=/;";
+// 	console.log(eventArray);
+// }
+
+function save(name, date, starttime, endtime){
+	var n = document.getElementById(name).value;
+	var d = document.getElementById(date).value;
+	var st = document.getElementById(starttime).value;
+	var et = document.getElementById(endtime).value;
+	var eventObj = {name:n, date:d, starttime:st, endtime:et, member:0};
+	// eventArray.push(eventObj);
+	// localStorage.eventRecord = JSON.stringify(eventArray);
+	var size = localStorage.length;
+	if(n!=(""&&"Enter a name here") && d!="" && st!="" && et!="" && st<et)
+	{
+		var str = JSON.stringify(eventObj);
+		size++;
+		localStorage.setItem(size,str);
+		alert("You add an event successfully");
+		load(size);
+	}
+	else if(st>=et)
+	{
+		alert("Please enter a reasonable time!");
+	}
+	else
+	{
+		alert("Please fill out all information.");
+	}
+	document.getElementById('name').value = "Enter a name here";
+	//console.log(size);//test size
+	displaySignUp();
 }
-function saveCrap(){
-	var name = document.getElementById("name").value;
-	sessionStorage.setItem(name,"234");
-	document.getElementById('name').value = "";
-	display();	
+function load(n){
+	var storedValue = localStorage.getItem(n);
+	console.log(storedValue);//test the localStorage;
+	//var ob = JSON.parse(storedValue);
+	//console.log(name);
+	return storedValue;
 }
 
-function display(){
-	var Eventlist = document.getElementById("Eventlist");
+function displayCreateEvent(){
+	document.getElementById('name').value = "Enter a name here";
+}
+function displaySignUp(){
+	var Eventlist = document.getElementById('Eventlist');
 	Eventlist.innerHTML = "We are ready have following event(s):" +"<br />";
-	for(var x=0;x<sessionStorage.length;x++)
+	for(var x=1;x<=localStorage.length;x++)
 	{
-		var EN = sessionStorage.key(x);
-		var D = sessionStorage.getItem(EN);
-		Eventlist.innerHTML += (x+1) + ". Event:" + EN + ". Time:" + D + "<br />";
+		var EN = load(x);
+
+		Eventlist.innerHTML += x + ". " + EN + "<br />";
 	}
 }
-
-
-
-function show(f)
-{
-	var x=f.hobby;
-	alert(x.value);
+function cleanlist(){
+	localStorage.clear();
 }
