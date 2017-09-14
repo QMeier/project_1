c867879b700bde5eff9a0c964317b9d9bdb914ff
Chong Tan <a46981@gmail.com>
2017-09-13 22:34:29 -0500

    I delete a  useless function

--- SUScript.js
+++ SUScript.js
@@ -1,17 +1,5 @@
 window.addEventListener("load", displaySignUp, false);
-function savestudent(name, event){
-	var n = document.getElementById(name).value;
-	var eventObj = {name:n, };
-	var str = JSON.stringify(eventObj);
-	localStorage.setItem(n,str);
-	//var storedValue = localStorage.getItem(n);
-	//console.log(storedValue);
-	load(n);
-}
-function loadstudent(n){
-	var storedValue = localStorage.getItem(n);
-	console.log(storedValue);
-}
+
 function displaySignUp(){
 	var Eventlist = document.getElementById('Eventlist');
 	Eventlist.innerHTML = "We are ready have following event(s):" +"<br />";
